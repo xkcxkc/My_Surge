8429df575272b011cfaaf91f6a1e1cee5f760b96
florist <79241548+xkcxkc@users.noreply.github.com>
2023-07-14 07:35:39 +0800

    Update qx_datecard.js

--- qx_datecard.js
+++ qx_datecard.js
@@ -21,7 +21,7 @@ var messages = targetDates
     .join('\n');
 
 $task.fetch({
-  url: "https://v1.hitokoto.cn/?c=a&c=b&c=k&max_length=16"
+  url: "https://v1.hitokoto.cn/?c=a&c=b&c=k&max_length=20"
 }).then(response => {
   try {
     // 尝试解析获取的数据
@@ -30,7 +30,7 @@ $task.fetch({
     let sentence = data.hitokoto;
     let source = data.from;
     // 发送通知，句子放在标题部分，出处放在消息部分
-    $notify(sentence, '', source ? ('—— ' + source + '\n' + messages) : messages);
+    $notify(sentence, '', source ? ('\t' +  '\t' +  '\t' +  '\t' + '———' + source + '\n' + messages) : messages);
   } catch (error) {
     // 如果解析数据时有错误，发送一个包含错误信息的通知
     $notify('解析数据时出错', '', error.toString());
